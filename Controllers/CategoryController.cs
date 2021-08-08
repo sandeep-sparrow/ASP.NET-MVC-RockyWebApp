4d99ff29f727cb59635c2fa59cdd3e2d24758f4a
sandeep-sparrow <sandeep.p4856@gmail.com>
2021-08-08 09:35:29 +0530

    Update Few Stylings and Added Delete Action in CategoryConstroller

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -71,10 +71,39 @@ namespace Rocky.Controllers
                 _db.Category.Update(obj);
                 _db.SaveChanges();
                 return RedirectToAction("Index");
-                // no need to define a constroller as it is in the same constroller
             }
             return View(obj);
 
+        }
+
+        // GET : Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST : Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
